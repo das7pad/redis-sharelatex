f9d233a095d590edc9249680da4d7183f58f0125
Jakob Ackermann <das7pad@outlook.com>
2020-11-09 11:39:10 +0100

    [misc] bump o-error to v3

--- Errors.js
+++ Errors.js
@@ -1,11 +1,7 @@
 const OError = require('@overleaf/o-error')
 
 class RedisError extends OError {}
-class RedisHealthCheckFailed extends RedisError {
-  constructor(message) {
-    super({ message })
-  }
-}
+class RedisHealthCheckFailed extends RedisError {}
 class RedisHealthCheckTimedOut extends RedisHealthCheckFailed {}
 class RedisHealthCheckWriteError extends RedisHealthCheckFailed {}
 class RedisHealthCheckVerifyError extends RedisHealthCheckFailed {}
